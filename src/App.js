d61d932ae330df0a254055d3da69c681667074bc
saifmathur <saifmathur41@gmail.com>
2020-07-09 04:36:36 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component { 
+  state={
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play FIFA'}
+    ]
+  }
+  render() {
+    return (
+      <div className="todo-app container">
+          <h1 className="center blue-text">Todo's</h1>
+          <Todos todos={this.state.todos} />
+      </div>
+    )
+  }
 }
-
 export default App;
