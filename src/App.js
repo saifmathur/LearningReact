06fe2c41c810032f43f477dab0ac91febc288f96
saifmathur <saifmathur41@gmail.com>
2020-07-10 03:50:24 +0530

    added delete module

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ class App extends React.Component {
       {id: 2, content: 'play FIFA'}
     ]
   }
+  deleteTodo = (id)=>{
+    console.log(id)
+    const todos = this.state.todos.filter(todo=>{
+      return todo.id !== id
+    })
+    this.setState({
+      todos
+    })
+  }
   render() {
     return (
       <div className="todo-app container">
           <h1 className="center blue-text">Todo's</h1>
-          <Todos todos={this.state.todos} />
+          <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     )
   }
